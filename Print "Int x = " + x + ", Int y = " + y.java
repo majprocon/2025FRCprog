dfeab61ff020efc4597ce8dcd871e1cc455f9a4d
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-06-18 18:06:22 -0500

    Create Print "Int x = " + x + ", Int y = " + y.java

---
+++ Print "Int x = " + x + ", Int y = " + y.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+         
+         int x = 10;
+         int y = 5;
+         int random = x;
+         
+         x = y;
+         y = random;
+         
+         System.out.println("Int x = " + x + ", Int y = " + y);
+    
+         
+    }
+}
