318f4a2a0e6097acd0000a7fd0ea604aa9ab5e41
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-06-18 18:08:55 -0500

    Create returnstatements.java

---
+++ returnstatements.java
@@ -0,0 +1,10 @@
+public class Example {
+
+    public static void sayHello() {
+        System.out.println("Hello, Majdoleen");
+    }
+    
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
