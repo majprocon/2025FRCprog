5824599f22202be328e76a85dd599ee34017b6b6
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-06-28 18:36:26 -0500

    Create multiplicationTable.java

---
+++ multiplicationTable.java
@@ -0,0 +1,12 @@
+public final class Main {
+    public static void main(String... args) {
+      int i, j;
+      int size = 10;
+        for (i = 1; i <= size; i++){
+          for (j = 1; j <= size; j++){
+             System.out.print(i * j + "\t");
+          }
+            System.out.print("\n");
+        }
+    }
+  }
