acd71f16c8d2b942428d66a7e59ce48b0dc05860
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-06-29 19:13:09 -0500

    Update CreateAnimalZoo.java

--- CreateAnimalZoo.java
+++ CreateAnimalZoo.java
@@ -1,20 +1,20 @@
 public final class Main {
   
-  public static void main(String[] args) {
-    Animal animal = new Animal("Leo", "Mammal" , 6);
-        System.out.println(animal.name);
-        System.out.println(animal.species);
-        System.out.println(animal.age);
+    public static void main(String[] args) {
+      Animal animal = new Animal("Leo", "Mammal" , 6);
+      System.out.println("My animals name is: " + animal.name);
+          System.out.println("My animals species is: " + animal.species);
+          System.out.println("My animals age is: " + animal.age);
+              }
             }
-          }
-          class Animal {
-            String name;
-            String species;
-            int age = 6;
-    
-        public Animal(String name, String species, int age) {
+class Animal {
+          String name;
+          String species;
+          int age = 6;
+      
+      public Animal(String name, String species, int age) {
           this.name = name;
           this.species = species;
           this.age = age;
-        }
-    }
+          }
+      }
