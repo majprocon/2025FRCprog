5d7ffee6ad445d5bd33b09d3062af5a474158974
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-07-06 15:33:41 -0500

    Update DigitSummatation.java

--- DigitSummatation.java
+++ DigitSummatation.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    long n = scanner.nextInt();
-    long m = scanner.nextInt();
-    long a = n%10;
-    long b = m%10;
-    System.out.println(a+b);
+    long n = scanner.nextLong();
+    long m = scanner.nextLong();
+
+    System.out.println((n%10) + (m%10));
   }
 }
