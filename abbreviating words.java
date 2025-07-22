6e687774a9359ac79f44cca65320ebd235711f27
Majdoleen <168360154+majprocon@users.noreply.github.com>
2025-07-21 19:28:12 -0500

    Update abbreviating words.java

--- abbreviating words.java
+++ abbreviating words.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
+ 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String[] input = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(input[0]);
-
-        for (int i = 1; i <= n; i++) {
-            String word = input[i];
-
-            if (word.length() <= 10) {
-                System.out.println(word);
-            } else {
+ 
+        int n = scanner.nextInt();
+        scanner.nextLine(); 
+ 
+        for (int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+ 
+            if (word.length() > 10) {
                 int count = word.length() - 2;
                 String abbr = word.charAt(0) + String.valueOf(count) + word.charAt(word.length() - 1);
                 System.out.println(abbr);
+            } else {
+                System.out.println(word);
             }
         }
-      
+ 
         scanner.close();
     }
 }
